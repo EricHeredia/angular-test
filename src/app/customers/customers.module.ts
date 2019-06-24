d2ff82dcb505fc9d4c4acc1b2a4f8edffe20f264
EricHeredia <EricDavidHeredia@gmail.com>
2019-06-24 13:48:58 -0500

    finished data binding section

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CustomersListComponent } from './customers-list/customers-list.component';
 import { CustomersComponent }  from './customers.component';
+import { FilterTextboxComponent } from './customers-list/filter-textbox.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
-  imports:      [ CommonModule ],
-  declarations: [ CustomersComponent ],
+  imports:      [ CommonModule, SharedModule ],
+  declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   exports: [ CustomersComponent ]
 })
 export class CustomersModule { }
