bd09c7ad4c87625cf0c5574f6585509d9d3468fe
EricHeredia <EricDavidHeredia@gmail.com>
2019-06-24 02:24:14 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+  `
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+  title: string;
+  constructor() { }
+
+  ngOnInit() {
+    // We call a service that dets us data
+    this.title = "Hello World"
+  }
+
 }
